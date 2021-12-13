6ef62e4b8ab3d5414db26c402499d2b608f651b4
mazeu <marciomazeu@hotmail.com>
2021-12-12 23:13:53 -0300

    Page Home Created

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,30 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
-
+import { Link } from "react-router-dom";
+import './home.css';
 function Home() {
   const [filmes, setFilmes] = useState([]);
 
   useEffect(() => {
+
     async function loadFilmes() {
       const response = await api.get("r-api/?api=filmes");
-      console.log(response.data);
+      setFilmes(response.data);
     }
     loadFilmes();
   }, []);
   return (
     <div className="container">
       <div className="lista-filmes">
-        {filmes.map((filme) =>{
-          return(
+        {filmes.map((filme) => {
+          return (
             <article key={filmes.id}>
-            
+              <strong>{filme.nome}</strong>
+              <img src={filme.foto} alt={filme.nome} />
+              <Link to="/" ></Link>
             </article>
-          )
+          );
         })}
-        
       </div>
     </div>
   );
--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //base URL > https://sujeitoprogramador.com/r-api/?api=filmes
 
 const api = axios.create({
-    baseURL: 'https://sujeitoprogramador.com/'
+    baseURL: 'https://sujeitoprogramador.com'
 });
 
 export default api;
